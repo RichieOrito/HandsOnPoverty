c42e9c45efc3452599923b60aa4ac391e1e30dad
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-03-31 17:15:13 +0300

    finalized authentication

--- changeapp/urls.py
+++ changeapp/urls.py
@@ -1,8 +1,10 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.welcome),
+    path('',views.welcome, name="main"),
     path('register/', views.registerPage, name="register"),
-    path('login/', views.loginPage, name="login")
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout")
 ]
--- changeapp/views.py
+++ changeapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 from .forms import CreateUserForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def registerPage(request):
@@ -11,6 +13,8 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account has been created for ' + user)
             
             return redirect('login')
     
@@ -18,11 +22,28 @@ def registerPage(request):
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
+    
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+            
     context={}
     return render(request, 'accounts/login.html', context)
 
 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
+@login_required(login_url='login')
 def welcome(request):
     return render(request, 'index.html')
 
