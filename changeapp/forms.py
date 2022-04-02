cc73ded43d63f862a8529957f63a7462a9818e1a
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-04-02 05:39:31 +0300

    Created forms to be used

--- changeapp/forms.py
+++ changeapp/forms.py
@@ -1,7 +1,7 @@
 from dataclasses import field, fields
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import Articles, User
 from django import forms
 
 
@@ -10,4 +10,20 @@ from django import forms
 class CreateUserForm(UserCreationForm):
     class Meta:
         model=User
-        fields=['username', 'email', 'password1', 'password2']        
+        fields=['username', 'email', 'password1', 'password2']   
+
+class ArticlesForm(forms.ModelForm):
+    class Meta:
+        model = Articles
+        exclude = ['author', 'date_posted']
+
+class ProfileForm(forms.ModelForm):
+    bio = forms.CharField(required=False)
+    class Meta:
+        model = User
+        fields = ('full_name', 'bio', 'contact')
+
+class UpdateArticlesForm(forms.ModelForm):
+    class Meta:
+        model = Articles
+        fields = ('title', 'post',)
