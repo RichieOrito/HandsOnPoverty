cdd901a430f41aeb432a244894345ba873d9f549
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-04-02 05:46:42 +0300

    Created different urls for the different views

--- changeapp/urls.py
+++ changeapp/urls.py
@@ -6,5 +6,12 @@ urlpatterns = [
     path('',views.welcome, name="main"),
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
-    path('logout/', views.logoutUser, name="logout")
+    path('logout/', views.logoutUser, name="logout"),
+    path('profile/<str:username>/', views.profile, name = 'profile'),
+    path('update_profile', views.update_profile, name="update_profile"),
+    path('post/article', views.article, name='newArticle'),
+    path('delete/article/<int:id>', views.delete_article, name = 'delete_article'),
+    path('edit/article/<int:id>', views.update_article, name ='update_article'),
+    path('article/<int:id>', views.single_article, name = 'single_article')
+
 ]
