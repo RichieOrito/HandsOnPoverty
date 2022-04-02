86bb062daf4539039b5599a4b47de46923a2a2d0
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-04-02 05:47:17 +0300

    Created several view methods

--- changeapp/views.py
+++ changeapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
-from .forms import CreateUserForm
+from .forms import CreateUserForm, ArticlesForm, ProfileForm, UpdateArticlesForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .models import User, Articles
 
 
 def registerPage(request):
@@ -24,16 +25,16 @@ def registerPage(request):
 def loginPage(request):
     
     if request.method == "POST":
-        username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password')
         
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
         
         if user is not None:
             login(request, user)
             return redirect('main')
         else:
-            messages.info(request, 'Username or password is incorrect')
+            messages.info(request, 'Email or password is incorrect')
             
     context={}
     return render(request, 'accounts/login.html', context)
@@ -45,6 +46,79 @@ def logoutUser(request):
 
 @login_required(login_url='login')
 def welcome(request):
-    return render(request, 'index.html')
+    articles = Articles.get_all_posts()
 
-# Create your views here.
+    return render(request, 'index.html', {'articles':articles} )
+
+#Profile view
+def profile(request, username):
+    form = ProfileForm
+    current_user = request.user
+    articles = Articles.objects.filter(author = current_user).all()
+    profile =  User.objects.filter(username = username).first()
+    context = {
+         "profile": profile, "form":form, "articles":articles
+    }
+    return render(request, 'profile.html', context)   
+    
+@login_required()
+def update_profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating profile')
+
+        return redirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required()
+def article(request):
+    current_user =  request.user
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit =False)
+            article.author = current_user
+            article.save()
+        return redirect('main')
+
+    else:
+        form = ArticlesForm()
+
+    return render(request, 'new_article.html', {"form": form})  
+
+@login_required()
+def single_article(request, id):
+    article = Articles.get_post_by_id(id)
+    return render (request, 'single_article.html', {"article":article})
+
+@login_required()
+def delete_article(request, id):
+    article = Articles.objects.filter(pk = id).first()
+    article.delete()
+    return redirect(request.META.get('HTTP_REFERER'), {'success': 'Article deleted successfully'})
+
+
+@login_required()
+def update_article(request, id):
+    if request.method == 'POST':
+        article = Articles.get_post_by_id(id)
+        form = UpdateArticlesForm(request.POST, instance=article)
+
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.save()
+            print('Update successful')
+            messages.success(request, 'Article updated Successfully')
+
+            return redirect(request.META.get('HTTP_REFERER'), {'success': 'Article updated Successfully'})
+        else:
+             print('Update unsuccessful')   
+
+    return redirect(request.META.get('HTTP_REFERER'), {'error': 'There was an error updating'})
+
+
+    
