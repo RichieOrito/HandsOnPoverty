037c787bb40eb489e03e9a18486ec88ddf03f62a
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-04-06 19:06:26 +0300

    Contains daraja api urls

--- changeapp/urls.py
+++ changeapp/urls.py
@@ -12,9 +12,17 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"),
     path('profile/<str:username>/', views.profile, name = 'profile'),
     path('update_profile', views.update_profile, name="update_profile"),
+    # path('update_avatar', views.update_avatar, name="update_avatar"),
     path('post/article', views.article, name='newArticle'),
     path('delete/article/<int:id>', views.delete_article, name = 'delete_article'),
     path('edit/article/<int:id>', views.update_article, name ='update_article'),
-    path('article/<int:id>', views.single_article, name = 'single_article')
+    path('article/<int:id>', views.single_article, name = 'single_article'),
+    path('article/comment/<int:id>', views.add_comment, name='add_comment'),
+    path('online/lipa', views.lipa_na_mpesa_online, name='lipa_na_mpesa'),
+    # register, confirmation, validation and callback urls
+    path('c2b/register', views.register_urls, name="register_mpesa_validation"),
+    path('c2b/confirmation', views.confirmation, name="confirmation"),
+    path('c2b/validation', views.validation, name="validation"),
+    path('c2b/callback', views.call_back, name="call_back"),
 
 ]
